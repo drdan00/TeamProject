455283274f1938d4bf9114a4e7508da80420c10e
Dan Everett <dme@cs.uga.edu>
2014-11-20 17:36:21 -0500

    Update PostedMessage.java
    Add spec comments

--- src/content/PostedMessage.java
+++ src/content/PostedMessage.java
@@ -8,6 +8,9 @@ public class PostedMessage {
      String posted;
      String author;
      String title;
+     /**
+      * Load the object with date posted, author, and text content.
+      */
      public PostedMessage(String posted, String author, String title, String text) {
 		super();
 		this.posted = posted;
@@ -15,15 +18,27 @@ public class PostedMessage {
 		this.title = title;
 		this.text = text;
 	}
+	/**
+	 * Returns the date and time at which this message was posted.
+	 */
 	public String getPosted() {
 		return posted;
 	}
+	/*
+	 * Returns the author of this message.
+	 */
 	public String getAuthor() {
 		return author;
 	}
+	/*
+	 * Returns the title of this posting.
+	 */
 	public String getTitle() {
 		return title;
 	}
+	/**
+	 * Returns the content of this posting
+	 */
 	public String getText() {
 		return text;
 	}
